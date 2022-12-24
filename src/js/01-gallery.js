3a06fe5510a4d4153c0801b6cb99a40f1f5688e6
olena-semenenko <elen.kinakh@gmail.com>
2022-12-24 18:34:33 +0100

    Added 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+createMarkup(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+function createMarkup(array) {
+  const markup = array.reduce((acc, item) => {
+    const { preview, original, description } = item;
+    acc += `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    return acc;
+  }, '');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
