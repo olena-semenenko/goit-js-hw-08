825c657a64850735fbe8c53778be0de5fe43f51f
olena-semenenko <elen.kinakh@gmail.com>
2022-12-25 14:24:04 +0100

    Added 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+var throttle = require('lodash.throttle');
+player.on('timeupdate', throttle(setCurrentTime, 1000));
+
+function setCurrentTime(e) {
+  const currentTime = JSON.stringify(e);
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+const getTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player
+  .setCurrentTime(getTime.seconds)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
